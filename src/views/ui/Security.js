cc75484ff91faacf09d15852664311a1a35dbd04
Harshit Gupta <harshithigh2001@gmail.com>
2023-08-08 01:46:12 +0530

    added axios and security apis

--- src/views/ui/Security.js
+++ src/views/ui/Security.js
@@ -1,6 +1,48 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import SecurityService from '../../services';
+import axios from "axios";
+
 
 function Security(props) {
+
+    const API_URL = "http://localhost:8081/api/v1";
+    const getAllSecurities = async () => {
+        const ans =  await axios.get(`${API_URL}/getAllSecurities`);
+        return ans.data;
+    
+    }
+    
+    const getSecurityById = async (id) => {
+        await axios.get(`${API_URL}/getSecurityById/${id}`)
+            .then((response) => {
+                return response.data;
+            })
+    }
+    
+    const getSecurityByUserId = async (userid) => {
+        await axios.get(`${API_URL}/getSecurityByUserId/${userid}`)
+            .then((response) => {
+                return response.data;
+            })
+    }
+    
+    const getSecurityByDateRange = async (fromDate, toDate) => {
+        await axios.get(`${API_URL}/getSecurityByDateRange/${fromDate}/${toDate}`)
+            .then((response) => {
+                return response.data;
+            })
+    }
+    
+    const getTradesBySecurityId = async (securityid) => {
+        await axios.get(`${API_URL}/getTradesBySecurityId/${securityid}`)
+            .then((response) => {
+                return response.data;
+            })
+    }
+    
+    useEffect(() => {
+
+    }, [])
     return (
         <div>
            HELLO SECURITY 
